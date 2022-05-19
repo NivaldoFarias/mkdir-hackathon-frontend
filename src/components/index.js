ebad2be7fc245c46a2e85c0fdc8dc7f6e8fd815d
Nivaldo Farias <badivia.f@gmail.com>
2022-05-19 18:54:02 -0300

    chore: config linters (eslint, prettier)

--- src/components/index.js
+++ src/components/index.js
@@ -4,16 +4,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useLocalStorage } from './../hooks/useLocalStorage';
 import TokenContext from './../hooks/TokenContext';
 
-export default function App() {
+function App() {
   const [token, setToken] = useLocalStorage('token', null);
 
   return (
     <TokenContext.Provider value={{ token, setToken }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<></>} />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<></>} />
+        </Routes>
+      </BrowserRouter>
     </TokenContext.Provider>
   );
 }
+
+export default App;
