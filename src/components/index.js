20457de88fc13348572967541ee3420379f0f596
Nivaldo Farias <badivia.f@gmail.com>
2022-05-20 21:07:59 -0300

    feat: add home page and set header structure

--- src/components/index.js
+++ src/components/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { TokenProvider } from './../hooks/TokenContext.js';
-import Test from './../components/Test.jsx';
+import Home from './../pages/Home';
 
 function App() {
   return (
     <TokenProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Test />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </TokenProvider>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+import TokenContext from '../hooks/TokenContext.js';
+
+import { FaEdit, FaUserAlt } from 'react-icons/fa';
+import { ImUser } from 'react-icons/im';
+
+import logo from './../assets/logo-alt.png';
+
+function Home() {
+  const { token, setToken } = useContext(TokenContext);
+
+  function buildHomePage() {
+    return (
+      <>
+        <header className='header-container'>
+          <figure className='logo'>
+            <img className='logo__image' src={logo} alt='hackathon logo' />
+            <figcaption className='logo__text'>
+              <span>mkdir</span>&nbsp;hackathon
+            </figcaption>
+          </figure>
+          <div className='nav'>
+            <FaEdit className='edit-icon'></FaEdit>
+            <FaUserAlt className='user-icon'></FaUserAlt>
+          </div>
+        </header>
+      </>
+    );
+  }
+
+  const homePage = buildHomePage();
+
+  return (
+    <>
+      <main id='home-page'>{homePage}</main>
+    </>
+  );
+}
+
+export default Home;
