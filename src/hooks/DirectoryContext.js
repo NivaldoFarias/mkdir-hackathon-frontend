b73582e79ad39dbc613abb2900cfa49318b1eb22
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-05-20 22:04:56 -0300

    get template from api

--- src/hooks/DirectoryContext.js
+++ src/hooks/DirectoryContext.js
@@ -1,35 +1,27 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const DirectoryContext = createContext();
 
 export function DirectoryProvider({ children }) {
-  const [content, setContent] = useState([
-    {
-      type: 'folder',
-      name: 'Public',
-      content: [],
-    },
-    {
-      type: 'folder',
-      name: 'Documents',
-      content: [],
-    },
-    {
-      type: 'folder',
-      name: 'Downloads',
-      content: [],
-    },
-    {
-      type: 'folder',
-      name: 'Pictures',
-      content: [],
-    },
-    {
-      type: 'folder',
-      name: 'Videos',
-      content: [],
-    },
-  ]);
+  const [content, setContent] = useState([]);
+  const URL = 'http://localhost:5000';
+
+  useEffect(() => {
+    const template = localStorage.getItem('content');
+
+    if (!template) {
+      axios
+        .get(`${URL}/template`)
+        .then(({ data }) => {
+          localStorage.setItem('content', JSON.stringify(data.template));
+          setContent(data.template);
+        })
+        .catch((err) => console.log(err));
+    }
+
+    if (template) setContent(JSON.parse(template));
+  }, []);
 
   return (
     <DirectoryContext.Provider value={{ content, setContent }}>
